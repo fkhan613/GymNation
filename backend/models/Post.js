85d41b7d373ff05ab11290e02ce8f247e14b7373
Farhan Khan <farhan.k2005@gmail.com>
2024-07-20 16:06:23 -0400

    added posts model

---
+++ backend/models/Post.js
@@ -0,0 +1,50 @@
+//Description: The posts model is essentially like a instagram post, users can create posts, like posts and comment on posts.
+
+const mongoose = require("mongoose");
+
+const postSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    caption: {
+      type: String,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    likes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    comments: [
+      {
+        userId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "User",
+          required: true,
+        },
+        text: {
+          type: String,
+          required: true,
+        },
+        createdAt: {
+          type: Date,
+          default: Date.now,
+        },
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Post = mongoose.model("Post", postSchema);
+
+module.exports = Post;
