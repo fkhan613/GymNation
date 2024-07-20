4bccfb57585812fbaa2fc80d5ba7b843c434bdb7
Farhan Khan <farhan.k2005@gmail.com>
2024-07-19 21:46:28 -0400

    installed cloudinary for storing images

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 GymNation is a vibrant community where fitness enthusiasts unite to inspire, support, and challenge each other, creating a healthier and stronger nation together.
+
+This is a basic readme, currently being used to keep track of the technologies used in the project. This will be updated as the project progresses.
+
+## Technologies Used
+- HTML
+- CSS
+- JavaScript
+- Tailwind CSS
+- MongoDB
+- Express
+- React
+- Node.js
+- Cloudinary
+
--- backend/models/User.js
+++ backend/models/User.js
@@ -1,31 +1,40 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema(
-    {
-        firstName: {
-            type: String,
-            required: true,
-        },
-        lastName: {
-            type: String,
-            required: true,
-        },
-        email: {
-            type: String,
-            required: true,
-        },
-        password: {
-            type: String,
-            required: true,
-        },
-        username: {
-            type: String,
-            required: true,
-        },
+  {
+    firstName: {
+      type: String,
+      required: true,
     },
-    {
-        timestamps: true,
-    }
+    lastName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    bio: {
+      type: String,
+    },
+    pfp: {
+      type: String,
+      default: "",
+    },
+  },
+  {
+    timestamps: true,
+  }
 );
 
 const User = mongoose.model("User", userSchema);
