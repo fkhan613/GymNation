e4eb86bca0f8ca1b5059078ba84636c8f2ff5b4a
Farhan Khan <farhan.k2005@gmail.com>
2024-07-30 17:49:25 -0400

    added a title to login page

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,8 +1,10 @@
 import logo from "../assets/gymnation-logo.png";
 import LoginForm from "../components/LoginPage/LoginForm";
 import Header from "../components/LoginPage/Header";
+import useTitle from "../hooks/useTitle";
 
 const LoginPage = () => {
+  useTitle("GymNation | Login");
   return (
     <div className="min-h-screen bg-gray-100 lg:bg-gray-100 flex flex-col justify-center py-10 sm:px-6 lg:px-8">
       <img className="mx-auto h-20 scale-150 w-auto" src={logo} alt="GymNation Logo" />
