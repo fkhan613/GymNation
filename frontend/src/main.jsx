e5f246de119dc9ca8befb80b3bd55d23279f922e
Farhan Khan <farhan.k2005@gmail.com>
2024-07-30 16:50:54 -0400

    added favicon

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,17 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 
+// Function to dynamically set the favicon
+const setFavicon = (url) => {
+  const link = document.createElement('link');
+  link.rel = 'icon';
+  link.href = url;
+  document.head.appendChild(link);
+};
+
+// Set the favicon
+setFavicon('../public/favicon.ico');
+
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <App />
