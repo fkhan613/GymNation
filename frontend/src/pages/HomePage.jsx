52f366833c18e8f6b1b20adbe861b8331f813614
Farhan Khan <farhan.k2005@gmail.com>
2024-08-02 23:00:19 -0400

    got the auto login done based on valid access token

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,4 +1,41 @@
+import { useEffect } from "react";
+
 const HomePage = () => {
+  //get all posts
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        // Retrieve the token from localStorage
+        const token = localStorage.getItem("accessToken");
+        console.log("Token:", token);
+
+        //fetch posts
+        const response = await fetch("http://localhost:3500/posts", {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+
+        //log the entire response
+        console.log("Response:", response);
+
+        //check if the response is ok
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        //parse the response
+        const responseData = await response.json();
+
+        //log the response data
+        console.log("Response Data:", responseData);
+      } catch (error) {
+        //log any errors
+        console.error("Error fetching posts:", error);
+      }
+    };
+    fetchPosts();
+  }, []);
 
   return <div>HomePage</div>;
 };
--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -4,9 +4,14 @@ import Header from "../components/LoginPage/Header";
 import useTitle from "../hooks/useTitle";
 import { useEffect } from "react";
 import { toast } from "react-toastify";
+import {jwtDecode} from "jwt-decode";
+import { useNavigate } from "react-router-dom";
+
 
 const LoginPage = () => {
   useTitle("Login | GymNation");
+
+  const navigate = useNavigate();
 
   useEffect(() => {
     const params = new URLSearchParams(window.location.search);
@@ -23,6 +28,29 @@ const LoginPage = () => {
       );
     }
   }, []);
+
+  useEffect(() => {
+    const token = localStorage.getItem("accessToken");
+
+    if (token) {
+      try {
+        const decodedToken = jwtDecode(token);
+
+        const currentTime = Date.now() / 1000; // Current time in seconds
+
+        if (decodedToken.exp > currentTime) {
+          navigate("/dashboard");
+        } else {
+          console.log("Token has expired");
+        }
+      } catch (error) {
+        console.log("Invalid token:", error);
+      }
+    }
+  }, [navigate]);
+
+
+
   return (
     <div className="min-h-screen bg-gray-100 lg:bg-gray-100 flex flex-col justify-center py-10 sm:px-6 lg:px-8">
       <img
