d5eae2959c8723b2d4ce87a112fd429226359c50
Farhan Khan <farhan.k2005@gmail.com>
2024-08-02 23:50:03 -0400

    made a very rough landing page, and put in the auto login in the register page as well

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,9 +1,105 @@
-import useTitle from '../hooks/useTitle'
-const LandingPage = () => {
-  useTitle('GymNation | Connecting Fitness Enthusiasts')
-  return (
-    <div>LandingPage</div>
-  )
-}
+import useTitle from "../hooks/useTitle";
+import { Link } from "react-router-dom";
 
-export default LandingPage
+const LandingPage = () => {
+  useTitle("GymNation | Connecting Fitness Enthusiasts");
+
+  return (
+    <div>
+      <svg
+        version="1.1"
+        xmlns="http://www.w3.org/2000/svg"
+        xmlnsXlink="http://www.w3.org/1999/xlink"
+        x="0px"
+        y="0px"
+        width="100%"
+        height="100%"
+        viewBox="0 0 1600 900"
+        preserveAspectRatio="xMidYMax slice"
+      >
+        <defs>
+          <linearGradient id="bg">
+            <stop
+              offset="0%"
+              style={{ stopColor: "rgba(255, 255, 255, 0.8)" }}
+            ></stop>
+            <stop
+              offset="50%"
+              style={{ stopColor: "rgba(255, 255, 255, 0.8)" }}
+            ></stop>
+            <stop
+              offset="100%"
+              style={{ stopColor: "rgba(255, 255, 255, 0.8)" }}
+            ></stop>
+          </linearGradient>
+          <path
+            id="wave"
+            fill="url(#bg)"
+            d="M-363.852,502.589c0,0,236.988-41.997,505.475,0
+    s371.981,38.998,575.971,0s293.985-39.278,505.474,5.859s493.475,48.368,716.963-4.995v560.106H-363.852V502.589z"
+          />
+        </defs>
+        <g>
+          <use xlinkHref="#wave" opacity=".3">
+            <animateTransform
+              attributeName="transform"
+              attributeType="XML"
+              type="translate"
+              dur="10s"
+              calcMode="spline"
+              values="270 230; -334 180; 270 230"
+              keyTimes="0; .5; 1"
+              keySplines="0.42, 0, 0.58, 1.0;0.42, 0, 0.58, 1.0"
+              repeatCount="indefinite"
+            />
+          </use>
+          <use xlinkHref="#wave" opacity=".4">
+            <animateTransform
+              attributeName="transform"
+              attributeType="XML"
+              type="translate"
+              dur="8s"
+              calcMode="spline"
+              values="-270 230;243 220;-270 230"
+              keyTimes="0; .6; 1"
+              keySplines="0.42, 0, 0.58, 1.0;0.42, 0, 0.58, 1.0"
+              repeatCount="indefinite"
+            />
+          </use>
+          <use xlinkHref="#wave" opacity="0.56">
+            <animateTransform
+              attributeName="transform"
+              attributeType="XML"
+              type="translate"
+              dur="6s"
+              calcMode="spline"
+              values="0 230;-140 200;0 230"
+              keyTimes="0; .4; 1"
+              keySplines="0.42, 0, 0.58, 1.0;0.42, 0, 0.58, 1.0"
+              repeatCount="indefinite"
+            />
+          </use>
+        </g>
+      </svg>
+
+      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center">
+        <h1 className="text-4xl font-bold text-gray-800">
+          Welcome to GymNation
+        </h1>
+        <p className="text-lg text-gray-700 mt-4">
+          Connecting Fitness Enthusiasts & Reaching New Limits
+        </p>
+        <div className="mt-8">
+          <Link
+            to="/register"
+            className="bg-indigo-600 text-white hover:bg-indigo-800 px-6 py-3 rounded-md text-lg font-medium transition duration-300"
+          >
+            Enter
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LandingPage;
--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -2,9 +2,35 @@ import useTitle from "../hooks/useTitle";
 import RegisterForm from "../components/RegisterPage/RegisterForm";
 import Header from "../components/RegisterPage/Header";
 import logo from "../assets/gymnation-logo.png";
+import { useEffect } from "react";
+import { jwtDecode } from "jwt-decode";
+import { useNavigate } from "react-router-dom";
 
 const RegisterPage = () => {
   useTitle("Register | GymNation");
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem("accessToken");
+
+    if (token) {
+      try {
+        const decodedToken = jwtDecode(token);
+
+        const currentTime = Date.now() / 1000; // Current time in seconds
+
+        if (decodedToken.exp > currentTime) {
+          navigate("/dashboard");
+        } else {
+          console.log("Token has expired");
+        }
+      } catch (error) {
+        console.log("Invalid token:", error);
+      }
+    }
+  }, [navigate]);
+
   return (
     <div className="min-h-screen bg-gray-100 lg:bg-gray-100 flex flex-col justify-center py-10 sm:px-6 lg:px-8">
       <img
