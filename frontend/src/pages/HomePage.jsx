8dc0688e21120b237940d2d9c10178b7896fe36c
Farhan Khan <farhan.k2005@gmail.com>
2024-08-03 21:48:40 -0400

    added more validation to redirect back to login screen if not logged in

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,10 +1,36 @@
 import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { jwtDecode } from "jwt-decode";
 import useTitle from "../hooks/useTitle";
 
 const HomePage = () => {
-  useTitle("Home | GymNation");
-  return <div>HomePage</div>;
 
+  useTitle("Home | GymNation");
+
+  const navigate = useNavigate();
+
+  // Check if the user is logged in
+  useEffect(() => {
+    const token = localStorage.getItem("accessToken");
+
+    if (!token) {
+      navigate("/login");
+    }
+
+    try {
+      const decodedToken = jwtDecode(token);
+
+      const currentTime = Date.now() / 1000; // Current time in seconds
+
+      if (!(decodedToken.exp > currentTime)) {
+        navigate("/login");
+      }
+    } catch (error) {
+      console.log("Invalid token:", error);
+    }
+  }, [navigate]);
+
+  return <div>HomePage</div>;
 };
 
 export default HomePage;
