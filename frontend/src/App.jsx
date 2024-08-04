80e483adbdf820658346fa2a09042d0e5959f79a
Farhan Khan <farhan.k2005@gmail.com>
2024-08-03 22:06:57 -0400

    just put the login validation in the App instead of every file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,8 +5,35 @@ import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import HomePage from "./pages/HomePage";
 import UserProfilePage from "./pages/UserProfilePage";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { jwtDecode } from "jwt-decode";
 
 function App() {
+
+  const navigate = useNavigate();
+
+  // Check if the user is logged in
+  useEffect(() => {
+    const token = localStorage.getItem("accessToken");
+
+    if (!token) {
+      navigate("/login");
+    }
+
+    try {
+      const decodedToken = jwtDecode(token);
+
+      const currentTime = Date.now() / 1000; // Current time in seconds
+
+      if (!(decodedToken.exp > currentTime)) {
+        navigate("/login");
+      }
+    } catch (error) {
+      console.log("Invalid token:", error);
+    }
+  }, [navigate]);
+
   return (
     <Routes>
       <Route path="/" element={<MainLayout />}>
