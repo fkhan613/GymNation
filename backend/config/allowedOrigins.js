5edf631ed57b564e0d65ab7e32744cea6a42ad44
Farhan Khan <farhan.k2005@gmail.com>
2024-09-20 22:29:55 -0400

    ..

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -2,7 +2,7 @@ const allowedOrigins = [
   "http://localhost:3000",
   "http://localhost:5173",
   "https://gymnation-2pf8.onrender.com",
-  "https://gymnation-app.netlify.app/",
+  "https://gymnation-app.netlify.app",
 ];
 
 module.exports = allowedOrigins;
