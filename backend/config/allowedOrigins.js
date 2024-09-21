f0ccf87a70e86536710043670635f111848c0280
Farhan Khan <farhan.k2005@gmail.com>
2024-09-20 21:35:24 -0400

    added allowed origins

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,3 +1,7 @@
-const allowedOrigins = ["http://localhost:3000", "http://localhost:5173"];
+const allowedOrigins = [
+  "http://localhost:3000",
+  "http://localhost:5173",
+  "https://gymnation-2pf8.onrender.com",
+];
 
 module.exports = allowedOrigins;
