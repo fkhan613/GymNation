d39192971ec2c5737d5e0c8af8269c35dc78696c
Farhan Khan <farhan.k2005@gmail.com>
2024-09-20 22:27:04 -0400

    added a new origin

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -2,6 +2,7 @@ const allowedOrigins = [
   "http://localhost:3000",
   "http://localhost:5173",
   "https://gymnation-2pf8.onrender.com",
+  "https://gymnation-app.netlify.app/",
 ];
 
 module.exports = allowedOrigins;
